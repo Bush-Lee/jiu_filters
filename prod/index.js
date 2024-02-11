42c96c8f5d96f2a0f4c8e1a858a21e272697058a
rares.tufa@flosslab.com <rares.tufa@flosslab.com>
2024-02-11 13:29:06 +0100

    Prod test

--- prod/index.js
+++ prod/index.js
@@ -1,167 +1,173 @@
-document.addEventListener('alpine:init', async() => {
-   Alpine.store("data", ({
-      all_files: [],
-      col_structure: [],
-      col_options: [],
-      form_data: {},
-      form_result: [],
-
-      async load(){
-         await load_col_structure();
-         await load_col_options();
-         await load_files();
-      },
-      update_form(option_name, value){
-         console.log(option_name, value)
-         if (!(option_name in this.form_data) ) {
-            this.form_data[option_name] = []
-            this.form_data[option_name].push(value)   
-         } else if (option_name in this.form_data) {
-            if (this.form_data[option_name].includes(value)){
-               this.form_data[option_name].pop(value)
-               return
-            }
-            this.form_data[option_name].push(value)   
+document.addEventListener("alpine:init", async () => {
+   Alpine.store("data", {
+     // Store data
+     all_files: [],
+     col_structure: [],
+     col_options: [],
+ 
+     // Form
+     form_result: [],
+ 
+     // Page data
+     current_option_index: 0,
+     current_option_select: "",
+ 
+     async load() {
+       await load_col_structure();
+ 
+       await load_col_options();
+       this.current_option_select = this.col_options[0];
+ 
+       await load_files();
+     },
+ 
+     // Form options
+     reset_form() {
+       for (const [index, element] of this.col_options.entries()) {
+         for (const [i, el] of element.options.entries()) {
+           if (el.checked) {
+             el.checked = false;
+           }
          }
-      },
-      calculate_form_result(){
-         this.form_result=JSON.stringify(this.form_data)
-         
-         col_structure = JSON.parse(JSON.stringify(this.col_structure))
-         form_data = JSON.parse(JSON.stringify(this.form_data))
-         all_files = JSON.parse(JSON.stringify(this.all_files))
-
-         result = []     
-         toPop = []
-         form_data = updateKeys(form_data, col_structure)
-         
-         for (const key in form_data) {
-            const values = form_data[key];
-            for (const file of all_files) {
-                if (Array.isArray(file[key])) {
-                    let good = true;
-                    for (const value of values) {
-                        if (!file[key].includes(value)) {
-                            good = false;
-                            break;
-                        }
-                    }
-                    if (good && !result.includes(file)) {
-                        result.push(file);
-                    } else if (!good && !toPop.includes(file)) {
-                        toPop.push(file);
-                    }
-                }
-            }
-        }
-        
-        for (const key in form_data) {
-            const values = form_data[key];
-            for (const file of all_files) {
-                if (!Array.isArray(file[key])) {
-                    for (const value of values) {
-                        if (value === file[key]) {
-                            if (!result.includes(file)) {
-                                result.push(file);
-                            }
-                        } else if (!toPop.includes(file)) {
-                            toPop.push(file);
-                        }
-                    }
-                }
-            }
-        }
-        
-        for (const res of result) {
-            for (const willPop of toPop) {
-                const index = result.indexOf(willPop);
-                if (index !== -1) {
-                    result.splice(index, 1);
-                }
-            }
-        }
-        
-         this.form_result = result
-      }   
-   }))
-})
-
-function doesObjectExist(list, targetObject) {
+       }
+ 
+       (this.current_option_index = 0),
+         (this.current_option_select = this.col_options[0]);
+     },
+ 
+     // Result calculator
+     get_form_options() {
+       checked_options = new Map();
+       for (const [index, element] of this.col_options.entries()) {
+         key = find_key_by_value(this.col_structure[0], element.name);
+         if (!checked_options.has(element.name)) {
+           checked_options[key] = [];
+         }
+         for (const [i, el] of element.options.entries()) {
+           if (el.checked) {
+             checked_options[key].push(el.value);
+           }
+         }
+       }
+       this.calculate_form_results(checked_options);
+     },
+ 
+     calculate_form_results(checked_options) {
+       result = new Array();
+       for (const file of this.all_files) {
+         // Tot rahatul asta de if ii inutil, dar mi-e lene sa si verific
+         // ar trebui sa ai doar pushul
+         result_index = find_index_by_key(result, file);
+         if (result_index == -1) {
+           result.push({
+             file: file,
+             i: 0,
+           });
+ 
+           result_index = result.length - 1;
+         }
+ 
+         for (let key in file) {
+           if (key == "Link") {
+             continue;
+           }
+ 
+           if (Array.isArray(file[key])) {
+             for (let value in file[key]) {
+               if (checked_options[key].includes(value)) {
+                 result[result_index].i += 1;
+               }
+             }
+           } else {
+             if (checked_options[key].includes(file[key])) {
+               result[result_index].i += 1;
+             }
+           }
+         }
+       }
+ 
+       result.sort((a, b) => a.i - b.i);
+       result = result.slice(-5);
+ 
+       file_result = new Array();
+       for (let res of result) {
+         file_result.push(res.file);
+       }
+       this.form_result = file_result;
+     },
+ 
+     // Page option parser
+     next_option() {
+       this.current_option_index += this.col_options.length - 1;
+       // TODO: Handle special cases
+       this.current_option_select = this.col_options[this.current_option_index];
+     },
+ 
+     previous_option() {
+       // TODO: Handle special cases
+       this.current_option_index -= 1;
+       this.current_option_select = this.col_options[this.current_option_index];
+     },
+   });
+ });
+ 
+ function find_index_by_key(list, key) {
    for (let i = 0; i < list.length; i++) {
-      if (objectsAreEqual(list[i], targetObject)) {
-         return true;
-      }
-   }
-   return false; 
-}
- 
-function objectsAreEqual(obj1, obj2) {
-   const keys1 = Object.keys(obj1);
-   const keys2 = Object.keys(obj2);
-
-   if (keys1.length !== keys2.length) {
-      return false;
-   }
-
-   for (let key of keys1) {
-      if (obj1[key] !== obj2[key]) {
-         return false;
-      }
-   }
-
-   return true;
-}
-
-function updateKeys(formData, colStructure) {
-   const updatedFormData = {};
- 
-   for (const [formKey, formValue] of Object.entries(formData)) {
-     const obj = colStructure[0]
-     const colKey = Object.keys(obj).find(
-       key => obj[key] === formKey
-     );
-
- 
-     if (colKey) {
-       updatedFormData[colKey] = formValue;
-     } else {
-       updatedFormData[formKey] = formValue;
+     if (list[i].key === key) {
+       return i;
      }
    }
+   return -1; // Return -1 if the key is not found in any object
+ }
  
-   return updatedFormData;
-}
-
-async function load_col_structure() {
-   col_structure = await getColDescription()
-   col_structure = JSON.parse(JSON.stringify(col_structure))
-   Alpine.store("data").col_structure = col_structure
-}
-
-async function load_files() {
-   final_data = await getAllFiles()
-   final_data = JSON.parse(JSON.stringify(final_data))
-   Alpine.store("data").all_files = final_data
-}
-
-async function load_col_options() {
-   data = await getColOptions()
-   data = JSON.parse(JSON.stringify(data))
-   
-   strucure_data = []
-   col_names = Alpine.store("data").col_structure
-   col_names = JSON.parse(JSON.stringify(col_names))
-   col_names = col_names[0]
-
-   for (key in col_names) {
-      strucure_data.push(
-         {
-            name: col_names[key],
-            options: data[key]
-         }
-      )
+ function find_key_by_value(obj, x) {
+   for (const key in obj) {
+     if (obj[key] === x) {
+       return key;
+     }
    }
-   
-   Alpine.store("data").col_options = strucure_data
-}
-
+   return null;
+ }
+ 
+ async function load_col_structure() {
+   col_structure = await getColDescription();
+   col_structure = JSON.parse(JSON.stringify(col_structure));
+   Alpine.store("data").col_structure = col_structure;
+ }
+ 
+ async function load_files() {
+   final_data = await getAllFiles();
+   final_data = JSON.parse(JSON.stringify(final_data));
+   Alpine.store("data").all_files = final_data;
+ }
+ 
+ async function load_col_options() {
+   data = await getColOptions();
+   data = JSON.parse(JSON.stringify(data));
+ 
+   strucure_data = [];
+   col_names = Alpine.store("data").col_structure;
+   col_names = JSON.parse(JSON.stringify(col_names));
+   col_names = col_names[0];
+ 
+   for (key in col_names) {
+     // TODO: Handle Link properly
+     if (key == "Link") {
+       continue;
+     }
+ 
+     for (const [index, element] of data[key].entries()) {
+       data[key][index] = {
+         value: element,
+         checked: false,
+       };
+     }
+ 
+     strucure_data.push({
+       name: col_names[key],
+       options: data[key],
+     });
+   }
+   Alpine.store("data").col_options = strucure_data;
+ }
+ 
